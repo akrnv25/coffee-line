a142a665addffcacb62564c91444623b63137631
akrnv25 <akrnv25@gmail.com>
2023-11-18 22:32:07 +0400

    renamed stores dir

--- client-ui-svelte/src/routes/layout.store.ts
+++ client-ui-svelte/src/routes/layout.store.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
-import { createNotificationsStore } from '../lib/stores/notifications';
+import { createNotificationsStore } from '../lib/store-creators/notifications';
 
 export const isLoading = writable(false);
 export const notifications = createNotificationsStore();
