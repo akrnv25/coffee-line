053a7e3e1a836286ac11928371e25e23133662c5
akrnv25 <akrnv25@gmail.com>
2023-11-18 14:23:19 +0400

    getting coffee on timeout; transition for coffee

--- client-ui-svelte/src/routes/catalog/+page.svelte
+++ client-ui-svelte/src/routes/catalog/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import { afterUpdate, getContext, onMount } from 'svelte';
+  import { fade } from 'svelte/transition';
   import ProductCard from '$lib/components/ProductCard.svelte';
   import Container from '$lib/components/Container.svelte';
   import Button from '$lib/components/Button.svelte';
@@ -8,14 +9,18 @@
   import { coffeeItems } from './catalog.store';
   import { isLoading } from '../layout.store';
 
-  let catalog;
-  let layoutContext = getContext('layout');
+  const layoutContext = getContext('layout');
+  const coffeeGettingTimeout = 30000;
+  let coffeeGettingTimeoutId = null;
 
   onMount(() => oneMoreCoffee());
   afterUpdate(() => layoutContext.scrollToBottom());
 
   function oneMoreCoffee() {
     isLoading.set(true);
+    if (coffeeGettingTimeoutId) {
+      clearTimeout(coffeeGettingTimeoutId);
+    }
     const id = $coffeeItems.length + 1;
     coffeeCollection.getById(id)
       .then(res => {
@@ -27,20 +32,31 @@
         coffeeItems.update((coffeeItems) => ([...coffeeItems, res.data]));
         isLoading.set(false);
       })
-      .catch(error => {
-        console.log(error);
+      .catch(err => {
+        console.log(err);
         isLoading.set(false);
+      })
+      .finally(() => {
+        coffeeGettingTimeoutId = setTimeout(oneMoreCoffee, coffeeGettingTimeout);
       });
   }
 </script>
 
 <Container maxWidth="640px">
   <div class="catalog">
-    {#each $coffeeItems as coffee (coffee.id)}
-      <ProductCard image={coffee.image} height="420px" width="320px">
-        <Coffee coffee="{coffee}" />
-      </ProductCard>
-    {/each}
+    {#if !$coffeeItems.length}
+      <div class="catalog__empty-message">
+        Please, click the button to add the first product to the catalog.
+      </div>
+    {:else}
+      {#each $coffeeItems as coffee (coffee.id)}
+        <div class="catalog__item" transition:fade>
+          <ProductCard image={coffee.image} height="420px" width="320px">
+            <Coffee coffee="{coffee}" />
+          </ProductCard>
+        </div>
+      {/each}
+    {/if}
     <div class="catalog__actions">
       <Button title="+" on:click={oneMoreCoffee} disabled={$isLoading} />
     </div>
@@ -54,11 +70,18 @@
     align-items: center;
   }
 
+  .catalog__empty-message {
+    margin: 8px 0;
+  }
+
+  .catalog__item {
+    margin: 4px 0;
+  }
+
   .catalog__actions {
-    width: 100%;
     display: flex;
     flex-direction: row;
     justify-content: center;
-    padding: 16px 0;
+    margin: 8px 0;
   }
 </style>
